56348eed55b0a395821473b1aae07b2a45610c98
Elvis Poenaru <elvis.poe96@yahoo.com>
2021-12-13 17:07:12 +0200

    Update show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,7 +6,7 @@
 
         <div class="col-12 col-md-4 offset-md-4">
             <div class="card">
-                <img class="card-img-top" src="https://picsum.photos/600/300" alt="Card image cap">
+                <img class="card-img-top" src="https://i.pravatar.cc/400?u={{ $post->id }}" alt="Card image cap">
                 <div class="card-body">
 
                     <h3>
